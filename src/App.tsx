02bb9a72dfddd5bea52ab1f71897273c80f869fd
Mari Zin <zin.marie.v@gmail.com>
2025-04-09 19:39:57 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import MyButton from './components/Logo'
+
+function App() {
+  return (
+  <h1>
+      <MyButton title='sfgs'></MyButton>
+  </h1>
+  )
+}
+
+export default App
